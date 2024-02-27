7b1b0ce43d3b0d41b13f5aa151c88772cebf3fbb
Parag G. <parag.gawde@capgemini.com>
2024-02-27 14:52:51 +0530

    refact.: Completed basic CRUD.

--- main.js
+++ main.js
@@ -8,14 +8,25 @@ const User = require('./models/User');
 const trainRoutes = require('./routes/train');
 const Train = require('./models/Train');
 
+const seatRoutes = require('./routes/seat');
+const Seat = require('./models/Seat');
+
+const reservationRoutes = require('./routes/reservation');
+const Reservation = require('./models/Reservation');
+
 const app = express();
 
 app.use(bodyParser.json());
+
 app.use('/user', userRoutes);
 app.use('/train', trainRoutes);
+app.use('/seat', seatRoutes);
+app.use('/reservation', reservationRoutes);
 
 User.sync();
 Train.sync();
+Seat.sync();
+Reservation.sync();
 
 app.listen(8000, () => {
     console.log('Listening on port 8000');
